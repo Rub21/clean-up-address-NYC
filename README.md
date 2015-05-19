5b4b9d0ca65fa886e9c4070c6b40e40fe3a28519
Rub21 <ruben@mapbox.com>
2015-05-19 11:52:31 +0530

    make urls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# clean-up-address-NYC
+# Clean up specialco adrees(A,B) in NYC
+
+Based in OverPass:
+
+`http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:xml][timeout:25];(node["addr:housenumber"="240-26"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716);way["addr:housenumber"="240-26"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716);relation["addr:housenumber"="240-26"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716);node["addr:housenumber"="240-20"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716);way["addr:housenumber"="240-20"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716);relation["addr:housenumber"="240-20"]["addr:street"="Knollwood Avenue"](40.764, -73.796,40.795, -73.716););out meta;>;out meta qt;`
--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 var fs = require('fs');
 var _ = require('underscore');
 var argv = require('optimist').argv;
-var request = require('request');
-var et = require('elementtree');
 var csv = require('csv-parser')
 
 var spreadsheets = [];
-var grid = JSON.parse(fs.readFileSync('grid-a-process.geojson', 'utf8'));
-
+var grid = JSON.parse(fs.readFileSync(argv.geofile, 'utf8'));
 _.each(grid.features, function(val) {
-	if (val.properties.PNTCNT === 323) {
-		var url = "http://api.openstreetmap.org/api/0.6/map?bbox=" + val.properties.left + "," + val.properties.top + "," + val.properties.right + "," + val.properties.bottom;
-		download_file(url, val.properties.PNTCNT + ".osm", val.geometry.coordinates, done);
-	}
+	get_url(val);
 });
 
-function download_file(url, localFile, coordinates, callback) {
-	var localStream = fs.createWriteStream(localFile);
-	var out = request({
-		uri: url
-	});
-	out.on('response', function(resp) {
-		if (resp.statusCode === 200) {
-			//console.log('Start download : ' + url);
-			out.pipe(localStream);
-			localStream.on('close', function() {
-				callback(localFile, coordinates, done);
-			});
-		}
-	});
-}
-
-function done(localFile, coordinates) {
-	//console.log(coordinates);
-	var XML = et.XML;
-	var ElementTree = et.ElementTree;
-	var element = et.Element;
-	var subElement = et.SubElement;
-	var data, etree;
-	data = fs.readFileSync(localFile).toString();
-	etree = et.parse(data);
-
-	var rqt = fs.createReadStream(argv.file)
+function get_url(val) {
+	var urls = [];
+	var url = 'http://127.0.0.1:8111/import?url=http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:xml][timeout:25];(';
+	var counter = 0;
+	var rqt = fs.createReadStream(argv.csvfile)
 		.pipe(csv())
 		.on('data', function(data) {
 			var point = [parseFloat(data.x), parseFloat(data.y)];
-			if (pointinpolygon(point, coordinates[0])) {
+			if (pointinpolygon(point, val.geometry.coordinates[0])) {
+				counter++;
+				var thenum = data.streetname.replace(/[^0-9]/g, '');
+				if (thenum !== '') {
+					data.streetname = data.streetname.replace(thenum, getGetOrdinal(thenum));
+				}
+				var bbox = [val.properties.top, val.properties.left, val.properties.bottom, val.properties.right];
+				var node = 'node["addr:housenumber"="' + data.house_num + '"]["addr:street"="' + data.streetname.toLowerCase().capitalize() + '"](' + bbox.toString() + ');';
+				var way = 'way["addr:housenumber"="' + data.house_num + '"]["addr:street"="' + data.streetname.toLowerCase().capitalize() + '"](' + bbox.toString() + ');';
+				var relation = 'relation["addr:housenumber"="' + data.house_num + '"]["addr:street"="' + data.streetname.toLowerCase().capitalize() + '"](' + bbox.toString() + ');';
+				url += node + way + relation;
+				if (counter === 20) {
+					url += ');out meta;>;out meta qt;';
+					urls.push(url);
+					url = 'http://127.0.0.1:8111/import?url=http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:xml][timeout:25];(';
+					counter = 0;
+				}
 
 			}
+
 		});
 
 
+	rqt.on('finish', function() {
+		url += ');out meta;>;out meta qt;';
+		urls.push(url);
+		for (var i = 0; i < urls.length; i++) {
+			if (urls[i].length > 133) {
+				var text = '- [ ] [' + val.properties.id + '-' + (i + 1) + '](' + urls[i] + ') \n';
+				if (val.properties.id <= 100) fs.appendFile(argv.csvfile.split('.')[0] + "-urls-1.csv", text, function(err) {});
+				if (val.properties.id > 100 && val.properties.id <= 200) fs.appendFile(argv.csvfile.split('.')[0] + "-urls-2.csv", text, function(err) {});
+				if (val.properties.id > 200 && val.properties.id <= 300) fs.appendFile(argv.csvfile.split('.')[0] + "-urls-3.csv", text, function(err) {});
+				if (val.properties.id > 300 && val.properties.id <= 400) fs.appendFile(argv.csvfile.split('.')[0] + "-urls-4.csv", text, function(err) {});
+				if (val.properties.id > 400) fs.appendFile(argv.csvfile.split('.')[0] + "-urls-5.csv", text, function(err) {});
+			}
 
+		};
+	});
 }
 
 function getGetOrdinal(n) {
@@ -74,3 +77,9 @@ function pointinpolygon(point, vs) {
 	}
 	return inside;
 }
+
+String.prototype.capitalize = function() {
+	return this.replace(/(^|\s)([a-z])/g, function(m, p1, p2) {
+		return p1 + p2.toUpperCase();
+	});
+};
